76891c83839aeb222e12436307e822282df56c6c
Azizbek <dilshodovichazizbek@gmail.com>
2020-12-16 13:45:00 +0500

    New changes for exist blades

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,81 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Perfect Logics</title>
-    <link
-            rel="stylesheet"
-            href="https://pro.fontawesome.com/releases/v5.8.2/css/all.css"
-            integrity="sha384-xVVam1KS4+Qt2OrFa+VdRUoXygyKIuNWUUUBZYv+n27STsJ7oDOHJgfF0bNKLMJF"
-            crossorigin="anonymous"
-    />
-    <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
-            rel="stylesheet"
-    />
-    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}" />
+    <title>Khorezm silk</title>
+    <link rel="stylesheet" href="{{ asset('css/fonts.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('vendor/Bootstrap/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/icons/icomoon/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/about.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/news.css') }}" />
-    @stack('styles')
-    <script src="{{ asset('vendor/bootstrap/js/popper.js') }}"></script>
-</head>
-
-<body>
-@include('partials._callback')
-<header>
-    <nav>
-        <a href="{{ route('landing-page') }}">
-            <img
-                class="logo"
-                src="{{ asset('img/logo.png') }}"
-            />
-        </a>
-
-        <div class="navbar-open-btn"><i class="fal fa-bars"></i></div>
-        <div class="right-nav">
-            <ul class="top-nav">
-                <li class="nav-email">
-                    <a href="#" class="nav-email-link">{{ setting('site.header_phone') }}</a>
-                </li>
-
-                <li class="nav-phone"><i class="fa fa-phone-office"></i> @lang('Request callback')</li>
-
-                <li class="nav-flag">
-                    @if(app()->getLocale() == 'en')
-                        <a href="{{ route('lang.switch', 'ru') }}">
-                            <div class="nav-flag-txt">РУС</div>
-                            <img src="{{ asset('img/RU flag 1.png') }}" alt="" />
-                        </a>
-                    @else
-                        <a href="{{ route('lang.switch', 'en') }}">
-                            <div class="nav-flag-txt">ENG</div>
-                            <img src="{{ asset('img/eng.png') }}" alt="" />
-                        </a>
-                    @endif
-                </li>
-
-                <div class="navbar-close-btn"><i class="fal fa-times"></i></div>
-            </ul>
-            @include('partials.menus.main')
+  </head>
+  <body>
+    <header>
+      <nav>
+        <div class="nav">
+          <a href="{{ route('landing-page') }}">
+            <img src="{{ asset('img/Logo.png') }}" alt="logo" />
+          </a>
+          @include('partials.menus.main')
         </div>
-    </nav>
-    @yield('slider')
-</header>
-@yield('content')
-<footer>
-    @include('partials.footer')
-</footer>
-<!-- Preloader -->
-<div id="preloader">
+        <div class="navbar-info">
+          <div class="navbar-info_phone">@lang('Marketing department')
+            <br />
+            <span>{{ setting('site.contact_first_phone') }}</span>
+          </div>
 
-</div>
-<!-- / Preloader -->
-<script src="{{ asset('vendor/jquery/JQuery.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.js') }}"></script>
-<script src="{{ asset('js/index.js') }}"></script>
-<script src="{{ asset('js/main.js') }}"></script>
-</body>
+
+          <div class="navbar-info_language">
+            <div class="dropdown">
+              <div
+                class="dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                @if(app()->getLocale() == 'en')ENG
+                @else РУС
+                @endif
+                
+              </div>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                @if(app()->getLocale() == 'en')
+                <a class="dropdown-item" href="{{ route('lang.switch', 'ru') }}">РУС</a>
+                @else
+                <a class="dropdown-item" href="{{ route('lang.switch', 'en') }}">ENG</a>
+                 @endif
+              </div>
+            </div>
+          </div>
+
+          <div class="navbar-info_callback">
+            <button>@lang('Request callback')</button>
+          </div>
+        </div>
+      </nav>
+
+    @if (\Request::is('/'))  
+      @include('partials.presentation')   
+    @endif 
+    
+    <!-- here should be fixed -->
+    <!-- yield('presentation') -->
+
+    </header>
+
+    @yield('content')
+
+    <footer>
+      @include('partials.footer')
+    </footer>
+
+  </body>
+
+  <!-- scripts -->
+  <script src="{{ asset('vendor/Jquery/JQuery.js') }}"></script>
+  <script src=" {{ asset('vendor/Bootstrap/js/popper.js') }}"></script>
+  <script src="{{ asset('vendor/Bootstrap/js/bootstrap.js') }}"></script>
+  <script src="https://kit.fontawesome.com/da87c04439.js"crossorigin="anonymous"></script>
 </html>
--- resources/views/partials/menus/main.blade.php
+++ resources/views/partials/menus/main.blade.php
@@ -1,17 +1,11 @@
-<ul class="navbar">
-    <li class="nav-item-one {{ (request()->is('/')) ? 'active' : '' }}">
-        <a class="nav-item-link" href="{{ route('landing-page') }}"><div>@lang('Home')</div></a>
-    </li>
-    <li class="nav-item-two {{ (request()->is('about-us')) ? 'active' : '' }}">
-        <a class="nav-item-link" href="{{ route('about') }}"><div>@lang('About')</div></a>
-    </li>
-    <li class="nav-item-three {{ (request()->is('products')) ? 'active' : '' }}">
-        <a class="nav-item-link" href="{{ route('shop.index') }}"><div>@lang('Products')</div></a>
-    </li>
-    <li class="nav-item-four {{ (request()->is('blog*')) ? 'active' : '' }}">
-        <a class="nav-item-link" href="{{ route('blog.list') }}"><div>@lang('News')</div></a>
-    </li>
-    <li class="nav-item-five {{ (request()->is('contact')) ? 'active' : '' }}">
-        <a class="nav-item-link" href="{{ route('contact.index') }}"><div>@lang('Contact')</div></a>
-    </li>
+<ul class="nav-menu">
+            <li class="{{ (request()->is('/')) ? 'active' : '' }}"><a href="{{ route('landing-page') }}">@lang('Home')</a></li>
+            <li class="{{ (request()->is('about-us')) ? 'active' : '' }}"><a href="{{ route('about') }}">@lang('About')</a></li>
+            <li class="{{ (request()->is('products')) ? 'active' : '' }}"><a href="{{ route('shop.index') }}">@lang('Products')</a></li>
+            <li class="{{ (request()->is('/')) ? 'active' : '' }}"><a href="#">@lang('Reviews')</a></li> 
+            <li class="{{ (request()->is('blog*')) ? 'active' : '' }}"><a href="{{ route('blog.list') }}">@lang('News')</a></li>
+            <li class="{{ (request()->is('/')) ? 'active' : '' }}"><a href="#">@lang('Gallery')</a></li>            
+            <li class="{{ (request()->is('contact')) ? 'active' : '' }}"><a href="{{ route('contact.index') }}">@lang('Contact')</a></li>
+
+            <!-- reviews and gallery should be checked for active again -->
 </ul>
