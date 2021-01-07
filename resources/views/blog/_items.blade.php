ab87361ddeeeeeaae5c61ae208496ad4e07716eb
Azizbek <dilshodovichazizbek@gmail.com>
2021-01-08 02:55:07 +0500

    Database changed for sorting the settings

--- resources/views/blog/_items.blade.php
+++ resources/views/blog/_items.blade.php
@@ -1,13 +1,23 @@
-@foreach($posts as $post)
-<div class="news-card" data-id="{{ $post->id }}">
-    <div class="news-card-img">
-        <img src="{{ Voyager::image($post->thumbnail('cropped')) }}" alt="fruit" />
+@foreach($posts as $item)
+  <div class="col-md-6 col-lg-4 px-0">
+    <div class="blogs-card" data-id="{{ $item->id }}">
+      <div class="b-card-header">
+        <img src="{{ Voyager::image($item->image) }}" alt="avatar" />
+        <div class="b-card-name">
+          <div class="b-card-name_name">Khorezm silk</div>
+          <div class="b-card-name_status">{{$item->t('created_at')->format('d.m.Y')}}</div>
+        </div>
+      </div>
+      <hr />
+      <div class="b-card-body">
+        <div class="b-card-body-title">
+          {{$item->t('title')}}
+        </div>
+        <div class="b-card-body-txt">
+          {{substr($item->t('excerpt'), 0, 180)}}. . .
+        </div>
+        <a href="{{ route('blog.show', $item->slug) }}" class="b-card-body-link"> @lang('Read more') </a>
+      </div>
     </div>
-    <div class="news-card-txt">
-        {{ $post->t('title') }}
-    </div>
-    <a href="{{ route('blog.show', $post->slug) }}">
-        <button class="news-card-btn"><div>@lang('Read more')</div></button>
-    </a>
-</div>
+  </div>
 @endforeach
